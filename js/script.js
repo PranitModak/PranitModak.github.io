2a8d9a4582b8f433076bd8ac05fa2f20daea5d67
Pranit Modak <empranit@gmail.com>
2019-03-31 21:00:30 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,9 +1,3 @@
 $(document).ready(function(){
-    $("#loc").hover(function(){
-        setTimeout(function(){ 
-            $("#loc").attr("src","System.gif");
-            $("#loc").removeClass("scale-up-center");
-            $("#loc").addClass("in-zoom");
-        }, 1300);
-    })
+    
 })
