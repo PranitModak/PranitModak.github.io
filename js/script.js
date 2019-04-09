aa08cf279d8481708823ef3559787582720eb907
PranitModak <43670565+PranitModak@users.noreply.github.com>
2019-04-09 17:58:37 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -14,8 +14,8 @@ $(document).ready(function () {
       changehead(1)
       txtchange("2012","10th from St. Stephen's School","Scored 91.5% in ICSE");
       setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},4000);
-      setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology.","Scored 8.25 DGPA");},10000);
-      setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence");setTimeout(function(){txttime()},5500)},16500);
+      setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology.","Scored 8.25 DGPA");},8000);
+      setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence");setTimeout(function(){txttime()},5500)},12000);
     }
     if($(".swiper-slide-active").attr("id")=="10") {
       changehead(2)
@@ -55,7 +55,7 @@ function txtchange(txt1,txt2,txt3) {
 }
 function txttime() {
   txtchange("2012","10th from St. Stephen's School","Scored 91.5% in ICSE");
-  setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},5500);
-  setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology","Scored 8.25 DGPA");},11000);
-  setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence"); setTimeout(function(){txttime()},5500)},16500);
+  setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},4000);
+  setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology","Scored 8.25 DGPA");},8000);
+  setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence"); setTimeout(function(){txttime()},5500)},12000);
 }
