45cb2589024766d569a9f393d6f9f57a13a94017
PranitModak <43670565+PranitModak@users.noreply.github.com>
2019-04-10 11:20:11 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -13,9 +13,9 @@ $(document).ready(function () {
     if($(".swiper-slide-active").attr("id")=="Timeline") {
       changehead(1)
       txtchange("2012","10th from St. Stephen's School","Scored 91.5% in ICSE");
-      setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},4000);
-      setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology.","Scored 8.25 DGPA");},9000);
-      setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence");setTimeout(function(){txttime()},6000)},14000);
+      setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},3890);
+      setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology.","Scored 8.25 DGPA");},10200);
+      setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence");setTimeout(function(){txttime()},5500)},16200);
     }
     if($(".swiper-slide-active").attr("id")=="10") {
       changehead(2)
@@ -55,7 +55,7 @@ function txtchange(txt1,txt2,txt3) {
 }
 function txttime() {
   txtchange("2012","10th from St. Stephen's School","Scored 91.5% in ICSE");
-  setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},4000);
-  setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology","Scored 8.25 DGPA");},9000);
-  setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence"); setTimeout(function(){txttime()},6000)},14000);
+  setTimeout(function(){txtchange("2014","12th from St. Stephen's School","Scored 92% in ISC");},5890);
+  setTimeout(function(){txtchange("2018","B.Tech from Techno India College of Technology","Scored 8.25 DGPA");},12600);
+  setTimeout(function(){txtchange("Now","Job at Cognizant (Programmer Analyst Trainee)","in Artificial Intelligence"); setTimeout(function(){txttime()},5500)},18800);
 }
