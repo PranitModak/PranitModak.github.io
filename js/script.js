4783c9af3a327799013ea18d74050ed380c97522
PranitModak <43670565+PranitModak@users.noreply.github.com>
2019-04-10 16:31:38 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -22,7 +22,6 @@ function sclmove(limit) {
   for(i=0;i<=limit;i++)
   document.dispatchEvent(keyEvent);
   changehead(limit+1);
-  txttime();
 }
 
 function sweep() {
@@ -31,10 +30,6 @@ function sweep() {
   }
   if ($(".swiper-slide-active").attr("id") == "Timeline") {
     changehead(1)
-    txtchange("2012", "10th from St. Stephen's School", "Scored 91.5% in ICSE");
-    setTimeout(function () { txtchange("2014", "12th from St. Stephen's School", "Scored 92% in ISC"); }, 3890);
-    setTimeout(function () { txtchange("2018", "B.Tech from Techno India College of Technology.", "Scored 8.25 DGPA"); }, 10200);
-    setTimeout(function () { txtchange("Now", "Job at Cognizant (Programmer Analyst Trainee)", "in Artificial Intelligence"); setTimeout(function () { txttime() }, 5500) }, 16200);
   }
   if ($(".swiper-slide-active").attr("id") == "Showcase") {
     changehead(2)
@@ -54,19 +49,4 @@ function sweep() {
   if ($(".swiper-slide-active").attr("id") == "contactinfo") {
     changehead(7)
   }
-}
-
-function txtchange(txt1, txt2, txt3) {
-  $("#txtcl1").html(txt1);
-  $("#txts1").html(txt1);
-  $("#txtcl2").html(txt2);
-  $("#txts2").html(txt2);
-  $("#txtcl3").html(txt3);
-  $("#txts3").html(txt3);
-}
-function txttime() {
-  txtchange("2012", "10th from St. Stephen's School", "Scored 91.5% in ICSE");
-  setTimeout(function () { txtchange("2014", "12th from St. Stephen's School", "Scored 92% in ISC"); }, 5890);
-  setTimeout(function () { txtchange("2018", "B.Tech from Techno India College of Technology", "Scored 8.25 DGPA"); }, 12600);
-  setTimeout(function () { txtchange("Now", "Job at Cognizant (Programmer Analyst Trainee)", "in Artificial Intelligence"); setTimeout(function () { txttime() }, 5500) }, 18800);
 }
